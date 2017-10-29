c6a5d76edd8c8cd524b3bb20f931c74f69625b41
Ata Gowani <atagowani@gmail.com>
2017-10-29 01:49:07 -0500

    Add gas price function

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,12 +7,13 @@ app.controller('InputController', ['$scope', '$log', '$http', function($scope, $
   $scope.cars = []
   $scope.result_final = {};
   $scope.userCar = "";
+  $scope.gasPrices = {};
 
+  // Request to get the cars
   $http({
     method: 'GET',
     url: './src/data/carData.json'
   }).then(function successCallback(response) {
-    console.log(response.data.Cars);
     response.data.Cars.forEach(function(car) {
       $scope.cars.push(car);
     }, this);
@@ -20,19 +21,32 @@ app.controller('InputController', ['$scope', '$log', '$http', function($scope, $
     console.log("ERROR:" + response);
   });
 
-  $scope.getPrice = function(Car, CrusingRange) {
-    var THEcar = [];
+  // Request to get the gas prices
+  $http({
+    method: 'GET',
+    url: 'http://www.fueleconomy.gov/ws/rest/fuelprices'
+  }).then(function successCallback(response) {
+    $scope.gasPrices = response.data;
+  }, function errorCallback(response) {
+    console.log("ERROR:" + response);
+  });
+
+  $scope.getPrice = function(Car, CrusingRange, gasType) {
+    var the_car = [];
+    var price_for_type = $scope.gasPrices[gasType.toLowerCase()];
+    console.log(price_for_type);
     $scope.cars.forEach(function(car) {
       if (car.make === Car) {
-        THEcar.push(car);
+        the_car.push(car);
       }
     }, this);
 
-    var max_miles = THEcar[0].mpg * THEcar[0].fuel_capacity;
+    var max_miles = the_car[0].mpg * the_car[0].fuel_capacity;
     var miles_used = max_miles - CrusingRange;
-    var gallons_to_fill = miles_used / THEcar[0].mpg;
-    console.log(gallons_to_fill);
-    return gallons_to_fill;
+    var gallons_to_fill = miles_used / the_car[0].mpg;
+    var price = gallons_to_fill * price_for_type;
+    console.log(price);
+    return price;
   }
 
 
