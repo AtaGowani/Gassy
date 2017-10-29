d4038d4c072156e1aa62396ff8bdb7517862c383
Ata Gowani <atagowani@gmail.com>
2017-10-29 01:08:49 -0500

    Remake everything

---
+++ src/js/controller.js
@@ -0,0 +1,40 @@
+'use strict'
+
+var app = angular.module('GassyApp', [])
+
+app.controller('InputController', ['$scope', '$log', '$http', function($scope, $log, $http){
+
+  $scope.cars = []
+  $scope.result_final = {};
+  $scope.userCar = "";
+
+  $http({
+    method: 'GET',
+    url: './src/data/carData.json'
+  }).then(function successCallback(response) {
+    console.log(response.data.Cars);
+    response.data.Cars.forEach(function(car) {
+      $scope.cars.push(car);
+    }, this);
+  }, function errorCallback(response) {
+    console.log("ERROR:" + response);
+  });
+
+  $scope.gallonsToFill = function(Car, CrusingRange) {
+    var THEcar = [];
+    $scope.cars.forEach(function(car) {
+      if (car.make === Car) {
+        THEcar.push(car);
+      }
+    }, this);
+
+    var max_miles = THEcar[0].mpg * THEcar[0].fuel_capacity;
+    var miles_used = max_miles - CrusingRange;
+    var gallons_to_fill = miles_used / THEcar[0].mpg;
+    console.log(gallons_to_fill);
+    return gallons_to_fill;
+  }
+
+
+
+}]);
--- src/js/dependencies.js
+++
@@ -1,8 +0,0 @@
-require('angular');
-require('auth0-js');
-require('angular-auth0');
-
-var Popper = require('popper.js');
-var $ = require('../../node_modules/jquery/jquery.min.js');
-require('bootstrap');
-
